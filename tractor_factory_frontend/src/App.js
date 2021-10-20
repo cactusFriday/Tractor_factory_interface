6f0b82a020c72fc122159b6412a0b1df39886a2f
Елизавета Царегородцева <eitsaregorodtseva@miem.hse.ru>
2021-10-20 17:55:24 +0300

    auth request

--- tractor_factory_frontend/src/App.js
+++ tractor_factory_frontend/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function LoginForm() {
+  const [login, setLogin] = useState("");
+  const [password, setPassword] = useState("");
 
-export default App;
+  const headers = {}
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      login: login,
+      password: password
+    };
+
+    axios.post(`https://jsonplaceholder.typicode.com/users`, { user }, {
+      headers: headers
+    })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+    }
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          </header>
+          <main className="App-main">
+            <div className="App-InputForm">
+              <p className="Title">Панель администратора</p>
+            <form onSubmit={handleSubmit}>
+              <label>
+                <input className="App-Input" 
+                type="text" 
+                name="login" 
+                placeholder="Логин"
+                value={login}
+                onChange={(e) => setLogin(e.target.value)}/>
+              </label>
+              <label>
+                <input className="App-Input" 
+                type="password" 
+                name="password" 
+                placeholder="Пароль"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}/>
+              </label>
+              <button className="App-Button" 
+              type="submit">
+                Войти
+                </button>
+            </form>
+            <a className="Link-Password" href="">Забыли пароль?</a>
+            </div>
+          </main>
+          <footer></footer>
+        </div>
+    );
+  }
