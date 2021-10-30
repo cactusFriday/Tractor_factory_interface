89d1e0676919a3651eec1f1484827b8fe8330997
ilya <ilja.kasatckin@yandex.ru>
2021-10-30 21:09:50 +0300

    second-prototype-authorization: change name app  DEFAULT_AUTHENTICATION_CLASSES

--- tractor_factory_interface/settings.py
+++ tractor_factory_interface/settings.py
@@ -62,7 +62,7 @@ REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'apps.authorization.backends.JWTAuthentication',
+        'authorization.backends.JWTAuthentication',
     ),
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
