62e559befec234140241a8a9d66cab6a4e2f1130
ilya <ilja.kasatckin@yandex.ru>
2021-10-30 23:17:57 +0300

    second-prototype-authorization: commit renderer

--- authorization/views.py
+++ authorization/views.py
@@ -31,7 +31,7 @@ class RegistrationAPIView(APIView):
 
 class LoginAPIView(APIView):
     permission_classes = (AllowAny,)
-    renderer_classes = (UserJSONRenderer,)
+    # renderer_classes = (UserJSONRenderer,)
     serializer_class = LoginSerializer
 
     def post(self, request):
