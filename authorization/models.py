7b8f4aa165b9a5e9e5dc4bae752b0095eea5abb7
ilya <ilja.kasatckin@yandex.ru>
2021-10-30 21:45:54 +0300

    second-prototype-authorization: set encode to model for token of model of user

--- authorization/models.py
+++ authorization/models.py
@@ -125,6 +125,6 @@ class User(AbstractBaseUser, PermissionsMixin):
             'exp': int(dt.strftime('%s'))
         }, settings.SECRET_KEY, algorithm='HS256')
 
-        return token
+        return token.encode("UTF-8")
 
 
