10e66fc63ee1c6e1251c12bf7c7e1142c216e948
Alexander <gnatyuk0alexander@gmail.com>
2021-11-01 00:40:59 +0300

    edit src/index.js App->Login

--- tractor_factory_frontend/src/index.js
+++ tractor_factory_frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Login from './components/Login';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
