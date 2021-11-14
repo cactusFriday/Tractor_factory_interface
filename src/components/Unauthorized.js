ce57af7420119dbbbd5193eeb804d168fccc55c4
Елизавета Царегородцева <eitsaregorodtseva@miem.hse.ru>
2021-11-14 18:05:42 +0300

    deploy: added button to unauthorized, changed history table styles

--- src/components/Unauthorized.js
+++ src/components/Unauthorized.js
@@ -1,10 +1,22 @@
-const Unauthorized = () => {
-    return (
-        <div style={{ textAlign: 'center', verticalAlign: 'middle', marginTop: '100px' }}>
-            <h1>Вы не авторизованы, чтобы просматривать данную страницу!</h1>
-        </div>
-    )
+import { useHistory } from "react-router-dom";
+import './style.css';
 
+const Unauthorized = () => {
+    const history = useHistory();
+
+    function pushToLogin() {
+        history.push("/");
+    }
+    
+        return(
+            <div style={{textAlign: 'center', verticalAlign: 'middle', marginTop: '100px'}}>
+                <h1>Вы не авторизованы, чтобы просматривать данную страницу!</h1>
+                <button class="btn btn-primary" style={{ marginTop: '50px'}}onClick={pushToLogin}>
+                    Перейти к авторизации</button>
+            </div>
+            
+        )
+    
 }
 
 export default Unauthorized;
