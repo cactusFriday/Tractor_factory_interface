036c22e45ac149c14aacb7aec42d0a8c527a0996
ilya <ilja.kasatckin@yandex.ru>
2021-11-15 00:07:36 +0300

    deploy-synhronization-post-buttons: created model buttons with many to many field and get buttons with status post

--- conveyor/urls.py
+++ conveyor/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from conveyor import views
+from conveyor.views import ButtonsBlocksRetrieveAPIView
 
 urlpatterns = [
-    path('', views.conveyor_state_list, name='conveyor-list'),
+    path('', views.posts_state_list, name='conveyor-list'),
+    path('update-list/', views.update_posts_status),
+    path('post-buttons/', ButtonsBlocksRetrieveAPIView.as_view()),
 ]
