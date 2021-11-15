bbc7022131c4a84c2ef3038c66e6fe59e9b395ec
ilya <ilja.kasatckin@yandex.ru>
2021-11-15 22:59:12 +0300

    deploy-synchronization-post-buttons: create change time solved when buttons_block change status after 'error'

--- conveyor/views.py
+++ conveyor/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
 from rest_framework.parsers import JSONParser
+from django.utils import timezone
 
 from conveyor.models import PostsState, ButtonsBlocks
 from accident.models import Accident
@@ -11,7 +12,7 @@ from conveyor.serializers import PostsStateSerializer, ButtonsBlocksSerializer, 
 @api_view(['GET'])
 def posts_state_list(request):
     """
-    GET:    достает состояния постов из БД и отправляет json
+    GET:    достает состояния блоков кнопок, которые связаны с постами из БД и отправляет json
     """
     if request.method == 'GET':
         conv_state_set = PostsState.objects.all()
@@ -21,6 +22,11 @@ def posts_state_list(request):
 
 @api_view(['POST'])
 def update_posts_status(request):
+    """
+    POST:   принимает номер блока кнопки и её статус и обновляет данные в БД.
+    Если статус блока error, то создаётся инцидент.
+    Если статус блока был error, а стал active или success, то инцидент становится решённым
+    """
     if request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = ButtonsBlocksSerializer(data=data, many=True)
@@ -32,6 +38,12 @@ def update_posts_status(request):
                         for post in buttons_block.posts.all():
                             accident = Accident(post=post.post_number)
                             accident.save()
+                    elif buttons_block.status_block == 'error':
+                        for post in buttons_block.posts.all():
+                            accidents = Accident.objects.filter(post=post.post_number)
+                            for accident in accidents:
+                                accident.time_solved = timezone.now()
+                                accident.save()
                     buttons_block.status_block = serializer.validated_data[i]['status_block']
                     buttons_block.save()
             return JsonResponse(serializer.data, status=201, safe=False)
