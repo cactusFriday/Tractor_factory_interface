7754233f02935f94c893b945f15013713736ae7a
ilya <ilja.kasatckin@yandex.ru>
2021-11-16 01:06:52 +0300

    deploy: change page size in queryset request

--- tractor_factory_interface/settings.py
+++ tractor_factory_interface/settings.py
@@ -72,7 +72,7 @@ REST_FRAMEWORK = {
     ),
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 200,
     'DEFAULT_THROTTLE_CLASSES': (
         'rest_framework.throttling.UserRateThrottle',
         'rest_framework.throttling.AnonRateThrottle',
