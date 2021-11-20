10182faa25d866939a57caaafb3238b48d07aa9e
Елизавета Царегородцева <eitsaregorodtseva@miem.hse.ru>
2021-11-21 00:08:18 +0300

    deploy: updated private routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { PrivateRoute } from './components/PrivateRoute';
+import {  PrivateRouteMonitoring, PrivateRouteUsers, PrivateRouteConfig  } from './components/PrivateRoute';
 import Login from './components/Login';
 import Monitoring from './components/Monitoring';
 import Register from './components/Register';
 import Unauthorized from './components/Unauthorized';
 import ConveyorConfig from './components/ConveyorConfig';
 import Users from './components/Users';
+import SessionExpired from './components/SessionExpired';
+import NotEnoughRights from './components/NotEnoughRights'; 
 
 export default class App extends Component {
   constructor(props) {
@@ -18,13 +20,15 @@ export default class App extends Component {
       return (
       <BrowserRouter>
       <div className="App" >
-      <Switch     >
+      <Switch>
       <Route exact path="/" component={ Login } />
       <Route exact path="/unauthorized" component={ Unauthorized } />
-      <PrivateRoute exact path="/monitoring" component={ Monitoring } />
-      <PrivateRoute exact path="/register" component={ Register } /> 
-      <PrivateRoute exact path="/config" component={ ConveyorConfig } />
-      <PrivateRoute exact path="/users" component={ Users } />
+      <Route exact path="/sessionexpired" component={ SessionExpired } />
+      <Route exact path="/notenoughrights" component={ NotEnoughRights } />
+      <PrivateRouteMonitoring exact path="/monitoring" component={ Monitoring } />
+      <PrivateRouteUsers exact path="/register" component={ Register } /> 
+      <PrivateRouteConfig exact path="/config" component={ ConveyorConfig } />
+      <PrivateRouteUsers exact path="/users" component={ Users } /> 
       </Switch>
       </div>
       </BrowserRouter>
